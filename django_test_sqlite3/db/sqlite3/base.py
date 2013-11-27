3665fe390811fdcd26a783c3a5b11fc9039b527f
Adam Chainz <adam@adamj.eu>
2013-11-27 16:38:29 +0000

    Complete Django 1.6 re-write with overriding of get_new_connection

--- django_test_sqlite3/db/sqlite3/base.py
+++ django_test_sqlite3/db/sqlite3/base.py
@@ -1,5 +1,22 @@
+from Queue import Queue
+
 from django.db.backends.sqlite3.base import (DatabaseError,
                                              IntegrityError,
                                              DatabaseFeatures,
                                              DatabaseOperations,
-                                             DatabaseWrapper)
+                                             DatabaseWrapper as OriginalDatabaseWrapper)
+
+
+CONNECTION_QUEUE = Queue()
+
+
+class DatabaseWrapper(OriginalDatabaseWrapper):
+    def get_new_connection(self, conn_params):
+        if CONNECTION_QUEUE.empty():
+            conn = super(DatabaseWrapper, self).get_new_connection(conn_params)
+            CONNECTION_QUEUE.put(conn)
+        else:
+            conn = CONNECTION_QUEUE.get()
+            CONNECTION_QUEUE.put(conn)
+
+        return conn
